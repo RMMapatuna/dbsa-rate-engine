49a1c8efcf5305257ca3ce1216d2ff7a978beb0e
Ravindu Mapatuna <Ravindum.mapatuna@gmail.com>
2025-03-25 12:32:43 +0530

    init

---
+++ src/main/java/com/digitalhub/rate_engine/RateEngineApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhub.rate_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateEngineApplication.class, args);
+	}
+
+}
